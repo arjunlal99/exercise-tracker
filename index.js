5e945e106b6a819f1dd7f16afe4b36d3a9f7103d
arjunlal99 <arjunlalma@gmail.com>
2020-09-05 11:53:19 +0530

    added new-user

--- index.js
+++ index.js
@@ -2,17 +2,48 @@ var express = require('express')
 var app = express()
 app.set('view engine', 'pug')
 require('dotenv').config()
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+var userSchema = new Schema({
+    username: String
+})
 
 var conn = mongoose.createConnection(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 conn.once('open', () => {
     console.log("MongoDB connection successful")
 })
 
+
+const User = conn.model('user', userSchema)
+
+
+
 app.get('/', (req,res) => {
     res.render('template')
 })
 
+
+app.post('/api/exercise/new-user', (req,res) => {
+    var user_instance = new User({
+        username: req.body.username
+    })
+    user_instance.save((err, docs) => {
+        if(err){
+            console.log(err)
+        }
+        else{
+            console.log(docs)
+            res.json({username: docs.username, _id: docs._id})
+        }
+    })
+})
+
+app.get('/api/exercise/users', (req,res) => {
+    
+})
 
 
 var listener = app.listen(process.env.PORT , () => {
