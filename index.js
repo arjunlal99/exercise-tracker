af54159535a8c709de1c9fe1c46ceebaba232076
arjunlal99 <arjunlalma@gmail.com>
2020-09-05 11:08:42 +0530

    commiting after adding start script

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ var app = express()
 app.set('view engine', 'pug')
 require('dotenv').config()
 var mongoose = require('mongoose')
+mongoose.createConnection(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+connect.once('open', () => {
+    console.log("MongoDB connection successful")
+})
 
 app.get('/', (req,res) => {
     res.render('template')
 })
+
+
 
 var listener = app.listen(process.env.PORT , () => {
     console.log('Listening at port ', listener.address().port)
